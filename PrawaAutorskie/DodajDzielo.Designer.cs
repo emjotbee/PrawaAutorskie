ce6b3f48cdb9c894e136230415625554b7b2b4a3
Kamil Kłonica <kamil.klonica@icloud.com>
2023-02-01 11:08:34 +0100

    0.1.7.8

--- PrawaAutorskie/DodajDzielo.Designer.cs
+++ PrawaAutorskie/DodajDzielo.Designer.cs
@@ -81,7 +81,7 @@ namespace PrawaAutorskie
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.Size = new System.Drawing.Size(260, 165);
             this.richTextBox1.TabIndex = 6;
-            this.richTextBox1.Text = "Opis";
+            this.richTextBox1.Text = "Implementacja funkcjonalności pozwalającej na";
             // 
             // dateTimePicker1
             // 
